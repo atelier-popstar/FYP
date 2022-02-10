f7cc82610e00658ee4c7be451e2e82a01d0d9b7b
Tom Tye <tyet@tcd.ie>
2022-02-10 23:00:21 +0000

    failed attempts to make reshape work
    still getting 'idvar' error
    -tried removing dupe headers to no success- needs more work
    -maybe look more into melt()

--- AEMT_Corp_Reverdy_Test/createAEMTDialLong.R
+++ AEMT_Corp_Reverdy_Test/createAEMTDialLong.R
@@ -1,8 +1,10 @@
 # Clearing Sets and Creating AEMTDialFULL
 # load mergedAEMT ### DO NOT FORGET TO REMOVE THE LINES THAT ARE SAME AS THE HEADER
 #Selecting one dialogue + Supression of unsuable column 
+#library(dplyr)
 
 mergedAEMT<- read.csv("mergedAEMT.csv",header=TRUE,sep="\t",fileEncoding="UTF-16LE")
+mergedAEMT[-seq(1, nrow(mergedAEMT), by = 966)]
 AEMTDialFULL<- mergedAEMT[-c(3,4,10,11,12)]
 
 names(AEMTDialFULL)[names(AEMTDialFULL) == 'GlobalIndex'] <- 'Speakers'
@@ -26,7 +28,12 @@ AEMTDialLONG <- reshape(data=AEMTDialFULL,
                         times=c("Speakers","Tokens","NGrams","SS","OS","N","DialID","Reality","Level"),
                         direction="long")
 
+#AEMTDialLONG <- data.table.melt(AEMTDialFULL, id.vars=c("GlobalIndex")
+                     
+                     
+#                     )
+
 connection <- file("C:/Users/ttye7/Desktop/4th Year/FYP/AEMT_Corp_Reverdy_Test/AnalysisR/AEMTDialFULL/AEMTDialFULL.DATA",open="at")
-write.table(AEMTDialFULL, connection, sep="\t")
+write.table(AEMTDialLONG, connection, sep="\t")
 close(connection)
 
