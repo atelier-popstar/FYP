cbdfd0244cf4a21cc81d57cf6379f25c2817cc79
Tom Tye <tyet@tcd.ie>
2022-03-10 18:03:27 +0000

    turn classification testing
    quick test to see if turn classification code was working

---
+++ Context Vector Analysis/turn_classification_test.py
@@ -0,0 +1,53 @@
+import os
+
+college = "C:/Users/tyet/Documents"
+home = "C:/Users/ttye7/Desktop/4th Year"
+
+inputpath = home + "/FYP Auxiliary/transcripts/Transcripts_Clean"
+
+class Sentence:
+    def __init__(self, global_index, local_index, speaker, sentence, eyecon, familiar):
+        self.local_index = local_index
+        self.speaker = speaker
+        self.sentence = sentence   
+        self.eyecon = eyecon
+        self.familiar = familiar
+        self.OS = 0
+        self.SS = 0
+
+os.chdir(inputpath)
+
+for transcript_no, file in enumerate(os.listdir()):
+
+    i = 0
+    filename = file
+    eyecon = "false"
+    familiar = "false"
+
+    if filename[2] == "e":
+        eyecon = "true"
+
+    if int(filename[1]) < 5:
+        if filename[4] == "3" or filename[4] == "4" or filename[4] == "7" or filename[4] == "8":
+            familiar = "true"
+
+    elif int(filename[1]) >= 5:
+        if filename[4] == "1" or filename[4] == "2" or filename[4] == "5" or filename[4] == "6":
+            familiar = "true"
+
+
+
+
+    input_file_path = inputpath + "/" + filename
+
+
+    with open(input_file_path) as fh:
+
+        for line in fh:
+
+            speaker, sentence = line.strip().split(None, 1)
+
+            print("\nGlobal Index: " + str(transcript_no + 1) + " Local Index: " + str(i) + " Speaker: " + speaker + " Familiar: " + familiar + " Eye Contact: " + eyecon)
+            i = i + 1
+
+        fh.close()
