3f393ed36742555bd787f77e4a7b8f2f87c6ab41
Tom Tye <tyet@tcd.ie>
2022-03-10 18:04:42 +0000

    sentence similarity expansion
    extending sentence_similarity to operate on all files for a given directory and append them to one master list
    -adding metadata tagging
    -changed definitions for sentence object accordingly
    TODO: csv output

--- Context Vector Analysis/sentence_similarity.py
+++ Context Vector Analysis/sentence_similarity.py
@@ -1,3 +1,4 @@
+import os
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -5,72 +6,97 @@ college = "C:/Users/tyet/Documents"
 home = "C:/Users/ttye7/Desktop/4th Year"
 
 model = SentenceTransformer('bert-base-nli-mean-tokens')
-input_file = home + "/FYP Auxiliary/transcripts/Transcripts_Clean/q1ec1.txt"
+inputpath = home + "/FYP Auxiliary/transcripts/Transcripts_Clean"
 
 class Sentence:
-    def __init__(self, index, speaker, sentence):
-        self.index = index
+    def __init__(self, global_index, local_index, speaker, sentence, eyecon, familiar):
+        self.local_index = local_index
+        self.global_index = global_index
         self.speaker = speaker
         self.sentence = sentence   
+        self.eyecon = eyecon
+        self.familiar = familiar
         self.OS = 0
         self.SS = 0
+sentences_master = []
+filename = ""
 
-sentences = []
-i = 0
+os.chdir(inputpath)
 
-with open(input_file) as fh:
+for transcript_no, file in enumerate(os.listdir()):
 
-    for line in fh:
+    i = 0
+    sentences = []
+    filename = file
+    eyecon = "false"
+    familiar = "false"
 
-        speaker, sentence = line.strip().split(None, 1)
+    if filename[2] == "e":
+        eyecon = "true"
 
-        sentences.append(Sentence(i, speaker, sentence.strip()))
+    if int(filename[1]) < 5:
+        if filename[4] == "3" or filename[4] == "4" or filename[4] == "7" or filename[4] == "8":
+            familiar = "true"
 
-for idx, sentence in enumerate(sentences):
-    sentence.vector = model.encode(sentence.sentence)
-    #print(sentence.vector.shape)
-    if idx != 0:
-        tmpspkr = sentence.speaker
-        #print("\n" + tmpspkr)
-        tmpidx = idx - 1
+    elif int(filename[1]) >= 5:
+        if filename[4] == "1" or filename[4] == "2" or filename[4] == "5" or filename[4] == "6":
+            familiar = "true"
 
-        if sentences[tmpidx].speaker == tmpspkr:
-            #print("\n Check 1")
-            #print(sentence.vector, sentences[tmpidx].vector)
-            sentence.SS = cosine_similarity(sentence.vector.reshape(1, -1), sentences[tmpidx].vector.reshape(1, -1))
-            tmpidx = tmpidx - 1
-            if tmpidx > 0:
-                while sentences[tmpidx].speaker == tmpspkr and tmpidx > 0:
-                    tmpidx = tmpidx - 1
-                    #print("\n Check 2 " + str(tmpidx))
+
+
+
+    input_file_path = inputpath + "/" + filename
+
+    #split input string & add metadata
+    with open(input_file_path) as fh:
+
+        for line in fh:
+
+            speaker, sentence = line.strip().split(None, 1)
+
+            sentences.append(Sentence(transcript_no, i, speaker, sentence.strip(), eyecon, familiar))
+            i = i + 1
+
+        fh.close()
+
+    #calculate vector mebeddings & compute SS & OS
+    for idx, sentence in enumerate(sentences):
+        sentence.vector = model.encode(sentence.sentence)
+        #print(sentence.vector.shape)
+        if idx != 0:
+            tmpspkr = sentence.speaker
+            #print("\n" + tmpspkr)
+            tmpidx = idx - 1
+
+            if sentences[tmpidx].speaker == tmpspkr:
+                #print("\n Check 1")
+                #print(sentence.vector, sentences[tmpidx].vector)
+                sentence.SS = cosine_similarity(sentence.vector.reshape(1, -1), sentences[tmpidx].vector.reshape(1, -1))
+                tmpidx = tmpidx - 1
+                if tmpidx > 0:
+                    while sentences[tmpidx].speaker == tmpspkr and tmpidx > 0:
+                        tmpidx = tmpidx - 1
+                        #print("\n Check 2 " + str(tmpidx))
                 
-                if sentences[tmpidx].speaker != tmpspkr:
-                    sentence.OS = cosine_similarity(sentence.vector.reshape(1, -1), sentences[tmpidx].vector.reshape(1, -1))
-                    #print("\n Check 3 ")
+                    if sentences[tmpidx].speaker != tmpspkr:
+                        sentence.OS = cosine_similarity(sentence.vector.reshape(1, -1), sentences[tmpidx].vector.reshape(1, -1))
+                        #print("\n Check 3 ")
 
-        elif sentences[tmpidx].speaker != tmpspkr:
-            #print("\n Check 1")
-            #print(sentence.vector, sentences[tmpidx].vector)
-            sentence.OS = cosine_similarity(sentence.vector.reshape(1, -1), sentences[tmpidx].vector.reshape(1, -1))
-            tmpidx = tmpidx - 1
-            if tmpidx > 0:
-                while sentences[tmpidx].speaker != tmpspkr and tmpidx > 0:
-                    tmpidx = tmpidx - 1
-                    #print("\n Check 2 " + str(tmpidx))
+            elif sentences[tmpidx].speaker != tmpspkr:
+                #print("\n Check 1")
+                #print(sentence.vector, sentences[tmpidx].vector)
+                sentence.OS = cosine_similarity(sentence.vector.reshape(1, -1), sentences[tmpidx].vector.reshape(1, -1))
+                tmpidx = tmpidx - 1
+                if tmpidx > 0:
+                    while sentences[tmpidx].speaker != tmpspkr and tmpidx > 0:
+                        tmpidx = tmpidx - 1
+                        #print("\n Check 2 " + str(tmpidx))
                 
-                if sentences[tmpidx].speaker == tmpspkr:
-                    sentence.SS = cosine_similarity(sentence.vector.reshape(1, -1), sentences[tmpidx].vector.reshape(1, -1))
-                    #print("\n Check 3 ")
+                    if sentences[tmpidx].speaker == tmpspkr:
+                        sentence.SS = cosine_similarity(sentence.vector.reshape(1, -1), sentences[tmpidx].vector.reshape(1, -1))
+                        #print("\n Check 3 ")
 
-    #print(sentence.speaker)
-    #print(sentence.sentence)
+    sentences_master.extend(sentences)
 
-for idx, sentence in enumerate(sentences):
-
-    print("\nSentence: " + str(idx))
-    print("\nSpeaker: " + sentence.speaker)
-    #print("\nEncoding: " + sentence.sentence)
-    print("\nOS: " + str(sentence.OS))
-    print("\nSS: " + str(sentence.SS))
-
-
+for sentence in sentences_master:
+    print("\nGlobal Index: " + str(sentence.global_index + 1) + " Local Index: " + str(sentence.local_index) + " Speaker: " + sentence.speaker + " Familiar: " + sentence.familiar + " Eye Contact: " + sentence.eyecon + " OS: " + sentence.OS + " SS: " + sentence.SS)
