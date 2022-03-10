bc6fcfbe039c5fc8c45bf850f3ec5d39c1f0fc77
Tom Tye <tyet@tcd.ie>
2022-03-10 16:04:15 +0000

    strip header lines update
    now works automatically for all transcripts

--- Context Vector Analysis/strip_header_lines.py
+++ Context Vector Analysis/strip_header_lines.py
@@ -1,14 +1,40 @@
-lines = []
-# read file
-with open(r"C:/Users/ttye7/Desktop/4th Year/FYP Auxiliary/transcripts/Transcripts/q1ec1.txt", 'r') as fp:
-    # read an store all lines into list
-    lines = fp.readlines()
+import os
 
-# Write file
-with open(r"C:/Users/ttye7/Desktop/4th Year/FYP Auxiliary/transcripts/Transcripts_Clean/q1ec1.txt", 'w') as fp:
-    # iterate each line
-    for number, line in enumerate(lines):
-        # delete line 5 and 8. or pass any Nth line you want to remove
-        # note list index starts from 0
-        if number not in [0,1,2]:
-            fp.write(line)
+inputpath = "C:/Users/ttye7/Desktop/4th Year/FYP Auxiliary/transcripts/Transcripts"
+outputpath = "C:/Users/ttye7/Desktop/4th Year/FYP Auxiliary/transcripts/Transcripts_Clean"
+lines = []
+filename = ""
+
+
+
+
+
+os.chdir(inputpath)
+
+#open transcripts directory
+for file in os.listdir():
+
+    if file.endswith(".txt"):
+
+        filename = file
+        
+        # read file
+        with open(inputpath + "/" + filename, 'r') as fp:
+            # read an store all lines into list
+            lines = fp.readlines()
+            fp.close()
+        
+        # write file
+        with open(outputpath + "/" + filename, 'w') as fp:
+            # iterate each line
+            for number, line in enumerate(lines):
+                # delete line 5 and 8. or pass any Nth line you want to remove
+                # note list index starts from 0
+                if number not in [0,1,2]:
+                    fp.write(line)
+            
+            fp.close()
+
+        
+        
+        
