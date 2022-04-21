abf8e6d056779c52e0b6b19e9a104b0b65f845f0
Tom Tye <tyet@tcd.ie>
2022-04-21 19:59:52 +0100

    mean averaging test fixes

--- Context Vector Analysis/mean_averaging_test.py
+++ Context Vector Analysis/mean_averaging_test.py
@@ -4,15 +4,18 @@ college = "C:/Users/tyet/Documents/GitHub"
 home = "C:/Users/ttye7/Desktop/4th Year"
 
 class Dialogue:
-    def __init__(self, global_id, reality, OS, SS):
+    def __init__(self, global_id, reality, eyecon, familiar, devscore, OS, SS):
         self.global_id = global_id
         self.reality = reality
+        self.eyecon = eyecon
+        self.familiar = familiar
+        self.devscore = devscore
         self.OS = OS
         self.SS = SS
 
 dialogues = []
 
-with open(home + "/FYP/Context Vector Analysis/results_v4.csv") as csv_file:
+with open(home + "/FYP/Context Vector Analysis/results_v3.csv") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     globalindextemp = 0
     OSaggregate = 0
@@ -34,7 +37,7 @@ with open(home + "/FYP/Context Vector Analysis/results_v4.csv") as csv_file:
                     SScnt += 1
             
             elif (int(row[0]) != globalindextemp):
-                dialogues.append(Dialogue(globalindextemp, int(row[6]), (OSaggregate/OScnt), (SSaggregate/SScnt)))
+                dialogues.append(Dialogue(globalindextemp, int(row[6]), row[3], row[4], int(row[5]), (OSaggregate/OScnt), (SSaggregate/SScnt)))
                 OSaggregate = 0
                 SSaggregate = 0
                 OScnt = 0
@@ -43,14 +46,14 @@ with open(home + "/FYP/Context Vector Analysis/results_v4.csv") as csv_file:
 
         linecnt += 1
 
-dialogues.append(Dialogue(globalindextemp, int(row[6]),  (OSaggregate/OScnt), (SSaggregate/SScnt)))
+dialogues.append(Dialogue(globalindextemp, int(row[6]), row[3], row[4], int(row[5]), (OSaggregate/OScnt), (SSaggregate/SScnt)))
 
 
-with open(home + "/FYP/Context Vector Analysis/Averaged_Similarity_Results_v2.csv", 'w', newline='',) as csvfile:
+with open(home + "/FYP/Context Vector Analysis/Averaged_Similarity_Results_SBERT.csv", 'w', newline='',) as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['Global Index', 'Reality', 'OS', 'SS'])
+    writer.writerow(['Global Index', 'Reality', 'EyeCon', 'Familiar', 'DevScore', 'OS', 'SS'])
     for dialogue in dialogues:
-        writer.writerow([dialogue.global_id, dialogue.reality, dialogue.OS, dialogue.SS])
+        writer.writerow([dialogue.global_id, dialogue.reality, dialogue.eyecon, dialogue.familiar, dialogue.devscore, dialogue.OS, dialogue.SS])
 
 
 
